4c3ecc7b6da3cdcf68e82160c59bbdcf7861cff4
James <meteg66@gmail.com>
2021-04-18 10:53:40 +0300

    Login OK

--- app/src/main/java/com/example/projekti/MainActivity.java
+++ app/src/main/java/com/example/projekti/MainActivity.java
@@ -2,38 +2,36 @@ package com.example.projekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "jes";
+    private final static String USER = "properties";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        laske();
 
-        Intent askAge = new Intent(this, StartActivity1.class);
+        // Tää on se storage mihin tietoja tallentuu tietoja
+        SharedPreferences sharedPrefs = getSharedPreferences(USER, MODE_PRIVATE);
+        Log.d(TAG, String.valueOf(sharedPrefs.contains("valuesSet")));
 
-
-
-        // Kysy ikä
-
-
-
-        // Kysy sp
-
-
-        // Kysy paino
-
-
-        startActivity(askAge);
-
-
-        Log.d(TAG, "kaikki ok");
-
+        // Tällä lauseella kattoo onko tiedot jo asetettu eli onko alkujutut tehty jo
+        if(sharedPrefs.getBoolean("valuesSet", false)) {
+            // Printtaa kaikki tiedot
+            Log.d(TAG, sharedPrefs.getAll().toString());
+            laske();
+        }
+        // Jos ei ole niin..
+        else {
+            Intent askAge = new Intent(this, StartActivity1.class);
+            startActivity(askAge);
+        }
     }
 
     public void laske() {
