fc3a6f45379cd7ff82ff104a680f34f28949fdcb
James <meteg66@gmail.com>
2021-05-05 11:52:22 +0300

    Javadocin lisäys

--- app/src/main/java/com/example/projekti/Singleton.java
+++ app/src/main/java/com/example/projekti/Singleton.java
@@ -3,22 +3,49 @@ package com.example.projekti;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <h1>Luokka joka pitää sisällään instanssin lisätyistä päivätiedoista</h1>
+ * Palauttaa sen hetkisen instanssin samana kaikkialle ohjelmassa
+ *
+ * @author Mete Güneysel
+ * @version 1.0 5/2021
+ */
+
 public class Singleton {
     private List<DayInfo> days;
     private static final Singleton instance = new Singleton();
 
-    public static Singleton getInstance(){
+    /**
+     * Palauttaa instanssin luokasta
+     *
+     * @return instanssi
+     */
+    public static Singleton getInstance() {
         return instance;
     }
 
-    private Singleton(){
+    /**
+     * Alustaa listan instanssille
+     */
+    private Singleton() {
         days = new ArrayList<>();
     }
 
-    public List<DayInfo> getAllDays(){
+    /**
+     * Palauttaa instanssiin lisätyt tiedot listana
+     *
+     * @return lista joka sisältää DayInfo olioita
+     */
+    public List<DayInfo> getAllDays() {
         return days;
     }
 
+    /**
+     * Palauttaa halutun päivän tiedot
+     *
+     * @param i indeksi jonka mukaan tietoa haetaan listalta
+     * @return palauttaa DayInfo olion
+     */
     public DayInfo getOneDay(int i) {
         return days.get(i);
     }
